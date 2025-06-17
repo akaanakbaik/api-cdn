53d821f375612ada5e5b645bfa42130a5f4af3b6
aka <khaliqarrasyidabdul@gmail.com>
2025-06-17 15:59:12 +0700

    Update static.ts

--- api/static.ts
+++ api/static.ts
@@ -1,9 +1,6 @@
-require('dotenv').config();
-
-module.exports = (req, res) => {
-  const parts = req.url.split('/');
-  const filename = parts.pop() || parts.pop(); // memastikan ambil nama file agar tidak tabrakan maut 🗿
-  const raw = `https://raw.githubusercontent.com/${process.env.GITHUB_USERNAME}/${process.env.GITHUB_REPO}/${process.env.GITHUB_BRANCH}/public/${filename}`;
-  res.writeHead(302, { Location: raw });
-  res.end();
-};
+export default function handler(req, res) {
+  const { url } = req;
+  const filePath = url.replace('/uploader/', '');
+  const rawUrl = `https://raw.githubusercontent.com/${process.env.GITHUB_USERNAME}/${process.env.GITHUB_REPO}/${process.env.GITHUB_BRANCH}/public/${filePath}`;
+  return res.redirect(302, rawUrl);
+}
